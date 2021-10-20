2258ecdb2eae126fb2b46ff671fdd78d8eb31b3f
emre <emrky.2018@icloud.com>
2021-10-20 14:59:53 +0300

    last update

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-function fibonacci(num) {
-    arr = [1,2]
-        for (i = 0; i < num  ; i++){
-            arr.push(arr[i] + arr[i + 1])
-        }
-        return arr[num-1];
-  }
-console.log(fibonacci(7))      
+let subtract1El = document.getElementById("subtract1");
+let numberVin1El = document.getElementById("numberVin1");
+let plus1El = document.getElementById("plus1");
+let subtract2El = document.getElementById("subtract2");
+let numberVin2El = document.getElementById("numberVin2");
+let plus2El = document.getElementById("plus2");
+let totalCheckEl = document.getElementById("totalCheck");
+let i = 1;
+let j = 1;
 
- 
+
+subtract1El.addEventListener("click", function(){
+    i--;
+    if(i <= 0){numberVin1El.innerHTML = 0;i = 0;}else{numberVin1El.innerHTML = i;};
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+
+})
+plus1El.addEventListener("click", function(){
+    i++;
+    if(i <= 0){numberVin1El.innerHTML = 0;i = 0;}else{numberVin1El.innerHTML = i;}
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+})
+subtract2El.addEventListener("click", function(){
+    j--;
+    if(j <= 0){numberVin2El.innerHTML = 0;j = 0;}else{numberVin2El.innerHTML = j;}
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+})
+plus2El.addEventListener("click", function(){
+    j++;
+    if(j <= 0){numberVin2El.innerHTML = 0;j = 0;}else{numberVin2El.innerHTML = j;}
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+})
