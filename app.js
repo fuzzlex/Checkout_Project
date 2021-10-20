6303714590f4585318c58c70ad4ce6ef7f8b4b01
emre <emrky.2018@icloud.com>
2021-10-20 15:08:16 +0300

    last update

--- app.js
+++ app.js
@@ -12,21 +12,21 @@ let j = 1;
 subtract1El.addEventListener("click", function(){
     i--;
     if(i <= 0){numberVin1El.innerHTML = 0;i = 0;}else{numberVin1El.innerHTML = i;};
-    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19).toFixed(2);
 
 })
 plus1El.addEventListener("click", function(){
     i++;
     if(i <= 0){numberVin1El.innerHTML = 0;i = 0;}else{numberVin1El.innerHTML = i;}
-    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19).toFixed(2);
 })
 subtract2El.addEventListener("click", function(){
     j--;
     if(j <= 0){numberVin2El.innerHTML = 0;j = 0;}else{numberVin2El.innerHTML = j;}
-    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19).toFixed(2);
 })
 plus2El.addEventListener("click", function(){
     j++;
     if(j <= 0){numberVin2El.innerHTML = 0;j = 0;}else{numberVin2El.innerHTML = j;}
-    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19);
+    totalCheckEl.innerHTML = "$" + Number(Number(i * 54.99) + Number(j * 74.99) + 19).toFixed(2);
 })
